96bf91eb9585389222173189423031a8747b1ac5
LeeSangAh <sangah941123@gmail.com>
2018-11-18 16:52:41 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+JS
+function doDisplay(){
+    var con = document.getElementById("result");
+    if(con.style.display=='none'){
+        con.style.display = 'block';
+    }else{
+        con.style.display = 'none';
+    }
+}
+
+
+function doDisplaybutton(){
+    var con = document.getElementById("operator");
+    if(con.style.display=='none'){
+        con.style.display = 'block';
+    }else{
+        con.style.display = 'none';
+    }
+}
